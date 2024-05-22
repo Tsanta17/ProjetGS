5647ab2cf473169f308908d0630ec1034999e738
Aina RDM <ainarakotoarimanana3@gmail.com>
2024-05-22 16:06:53 +0200

    Ajouts des tables

---
+++ database/migrations/2024_05_22_134135_create_commandes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id('commande_id');
+            $table->unsignedBigInteger('fournisseur_id');
+            $table->unsignedBigInteger('site_id');
+            $table->date('date_commande')->nullable();
+            $table->string('statut');
+            $table->boolean('abonnement')->default(false);
+            $table->unsignedBigInteger('budget_disponible');
+            $table->timestamps();
+
+            // Définir les clés étrangères
+            $table->foreign('fournisseur_id')->references('fournisseur_id')->on('fournisseurs')->onDelete('cascade');
+            $table->foreign('site_id')->references('site_id')->on('sites')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commandes');
+    }
+};
